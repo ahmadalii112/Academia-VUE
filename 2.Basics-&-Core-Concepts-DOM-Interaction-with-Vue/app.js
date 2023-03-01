45e41b18e24ae940156e27d4030e0960251ca8ae
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-03-01 23:06:01 +0500

    5. Understanding methods in Vue Apps

--- 2.Basics-&-Core-Concepts-DOM-Interaction-with-Vue/app.js
+++ 2.Basics-&-Core-Concepts-DOM-Interaction-with-Vue/app.js
@@ -4,6 +4,15 @@ const app = Vue.createApp({
       courseGoal: "Finish the course and learn vue!",
       vueLink: "https://vuejs.org",
     };
+  },
+  /*Methods allows function which should execute e.g like button click*/
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      return (randomNumber < 0.5)
+          ? "Learn Vue"
+          : "Master Vue"
+    }
   }
 
 
