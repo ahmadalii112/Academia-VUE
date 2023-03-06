adf97928144a34a6a4c667e00f3e01cf1721ec30
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-03-06 21:49:27 +0500

    Understanding Event Binding

--- 2.Basics-&-Core-Concepts-DOM-Interaction-with-Vue/app.js
+++ 2.Basics-&-Core-Concepts-DOM-Interaction-with-Vue/app.js
@@ -1,21 +1,9 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoalA: "Finish the course and learn vue!",
-      courseGoalB: "<h4>Finish the course and master vue!</h4>",
-      vueLink: "https://vuejs.org",
+      counter: 0,
     };
   },
-  /*Methods allows function which should execute e.g like button click*/
-  methods: {
-    outputGoal() {
-      const randomNumber = Math.random();
-      return (randomNumber < 0.5)
-          ? this.courseGoalA
-          : this.courseGoalB
-    }
-  }
-
-
 });
-app.mount("#user-goal");
+
+app.mount('#events');
