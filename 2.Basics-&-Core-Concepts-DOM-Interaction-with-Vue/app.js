47d46c8be5263e333a5a21d8b9c35ee7c3c8d172
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-03-06 22:22:37 +0500

    Using the Native Event Object

--- 2.Basics-&-Core-Concepts-DOM-Interaction-with-Vue/app.js
+++ 2.Basics-&-Core-Concepts-DOM-Interaction-with-Vue/app.js
@@ -2,9 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "Ahmad",
     };
   },
   methods: {
+    /* when you point out a function an argument is also passed by default in JS
+    setName(event){
+        this.name = event.target.value;
+      },*/
+    setName(event, lastName) {
+      this.name = `${event.target.value}  ${lastName}`;
+    },
     add(num) {
       this.counter += num
     },
