d24a15fd1781b3220762d56bf6a8cd24545db375
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-03-10 11:56:27 +0500

    Watchers 1.1

--- 2.Basics-&-Core-Concepts-DOM-Interaction-with-Vue/app.js
+++ 2.Basics-&-Core-Concepts-DOM-Interaction-with-Vue/app.js
@@ -2,17 +2,29 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 10,
-      name: "",
+      name: "", // Whenever Name Changes The Watcher Method will Re-Execute
+      fullName: "",
     };
   },
+  // You repeat data and computed property name as a method Name
+  watch: {
+    // name(newValue, OldValue)
+    name(value) {
+      if (value === "") {
+        this.fullName =  "";
+      }else {
+        this.fullName = value + ' ' + "Ali"
+      }
+    }
+  },
   computed: {
-    fullName() {
+    /*fullName() {
       console.log("Running Again!...");
       if (this.name === "") {
         return "";
       }
       return this.name + " " + "Ali";
-    },
+    },*/
   },
   methods: {
     /* when you point out a function an argument is also passed by default in JS
