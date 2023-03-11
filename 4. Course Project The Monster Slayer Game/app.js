151dafd0da038a23194befdbf531c86c34d461f3
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-03-11 19:45:09 +0500

    3. Updating the Health Bars

--- 4. Course Project The Monster Slayer Game/app.js
+++ 4. Course Project The Monster Slayer Game/app.js
@@ -10,7 +10,14 @@ const app = Vue.createApp({
       playerHealth: 100,
     }
   },
-  computed: {},
+  computed: {
+    monsterBarStyles() {
+      return {width: this.monsterHealth + '%'}
+    },
+    playerBarStyles() {
+      return {width: this.playerHealth + '%'}
+    },
+  },
   methods: {
     attackMonster() {
       /*
@@ -19,13 +26,13 @@ const app = Vue.createApp({
        decimalValue(randomNumber * (maxValue - minValue) + minValue)
       */
       // const attackValue = Math.floor(Math.random() * (12 - 5)) + 5
-      const attackValue = getRandomValue(12, 5)
+      const attackValue = getRandomValue(5, 12)
       this.monsterHealth = this.monsterHealth - attackValue;
       this.attackPlayer(); // When we attack monster then it attack back to player
     },
     attackPlayer() {
       // const attackValue = Math.floor(Math.random() * (15 - 8)) + 8
-      const attackValue = getRandomValue(12, 5)
+      const attackValue = getRandomValue(8, 15)
       this.playerHealth -= attackValue;
 
     }
