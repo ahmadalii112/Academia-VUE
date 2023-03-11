82c7127dbcb00ef4522849ef0dfd53dfdd445824
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-03-11 13:42:48 +0500

    Dynamic Styling with Inline Styles

--- 2.Basics-&-Core-Concepts-DOM-Interaction-with-Vue/app.js
+++ 2.Basics-&-Core-Concepts-DOM-Interaction-with-Vue/app.js
@@ -1,74 +1,25 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 10,
-      name: "", // Whenever Name Changes The Watcher Method will Re-Execute
-      lastName: "",
-      // fullName: "",
-    };
-  },
-  // You repeat data and computed property name as a method Name
-  watch: {
-    // watchers shines for these kind of behaviours we want
-    counter(value){
-      if (value >= 50 || value <= 0) {
-        const that = this;
-        setTimeout(function (){
-         that.counter = 0;
-        }, 2000)
-      }
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
     }
-    // name(newValue, OldValue)
-   /* name(value) {
-      if (value === "") {
-        this.fullName =  "";
-      }else {
-        this.fullName = value + ' ' + this.lastName
-      }
-    },
-    lastName(value){
-      if (value === "") {
-        this.fullName =  "";
-      }else {
-        this.fullName = this.name + ' ' + value
-      }
-    }*/
-  },
-  computed: {
-    fullName() {
-      console.log("Running Again!...");
-      if (this.name === "" || this.lastName === "") {
-        return "";
-      }
-      return this.name + " " + this.lastName;
-    },
   },
   methods: {
-    /* when you point out a function an argument is also passed by default in JS
-    setName(event){
-        this.name = event.target.value;
-      },*/
-    outputFullName() {
-      console.log("Running Again!...");
-      if (this.name === "") {
-        return "";
+    boxSelected(box) {
+      if (box === 'A') {
+        this.boxASelected = true
+        console.log(this.boxASelected, 'A');
+      } else if (box === 'B') {
+        this.boxBSelected = true
+        console.log(this.boxASelected, 'B');
+      } else if (box === 'C') {
+        this.boxCSelected = true
+        console.log(this.boxASelected, 'C');
       }
-      return this.name + " " + "Ali";
-    },
-    setName(event) {
-      this.name = `${event.target.value}`;
-    },
-    add(num) {
-      this.counter += num
-    },
-    reduce(num) {
-      this.counter -= num
-    },
-
-    resetInput() {
-      this.name = "";
     }
   }
 });
 
-app.mount('#events');
+app.mount('#styling');
