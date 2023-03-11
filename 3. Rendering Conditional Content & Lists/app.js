a131706dba4462f6393768c8e717c7122f824e5d
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-03-11 18:09:49 +0500

    8. Removing List Items

--- 3. Rendering Conditional Content & Lists/app.js
+++ 3. Rendering Conditional Content & Lists/app.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
   data() {
     return {
       enteredGoalValue: '',
-      goals: [],
+      goals: ["learn Vue", "Master Vue", "Finish The Course"],
       person: {
         'Name': 'Ahmad',
         'Age': 25,
@@ -12,8 +12,14 @@ const app = Vue.createApp({
   },
   methods: {
     addGoal() {
+      if (this.enteredGoalValue === '') {
+        return;
+      }
       this.goals.unshift(this.enteredGoalValue)
       this.enteredGoalValue = ''
+    },
+    removeGoal(idx) {
+      this.goals.splice(idx, 1);
     }
   }
 });
