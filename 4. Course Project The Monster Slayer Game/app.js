f0e6a531b8611c5425f166526dd5ac7f2fea954b
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-03-30 21:59:46 +0500

    5. Adding a Heal Functionality

--- 4. Course Project The Monster Slayer Game/app.js
+++ 4. Course Project The Monster Slayer Game/app.js
@@ -47,6 +47,18 @@ const app = Vue.createApp({
       const attackValue = getRandomValue(10, 25)
       this.monsterHealth = this.monsterHealth - attackValue;
       this.attackPlayer();
+    },
+    healPlayer() {
+      this.currentRound++
+      const healValue = getRandomValue(8, 20)
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+
+
     }
   },
 });
