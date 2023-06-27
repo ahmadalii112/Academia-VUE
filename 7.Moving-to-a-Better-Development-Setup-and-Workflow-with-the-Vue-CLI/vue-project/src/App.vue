a0b70fb666abc48c56519578e90d00894cbaa03e
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-06-27 20:47:10 +0500

    10. Demo Adding Components & Connecting Them

--- 7.Moving-to-a-Better-Development-Setup-and-Workflow-with-the-Vue-CLI/vue-project/src/App.vue
+++ 7.Moving-to-a-Better-Development-Setup-and-Workflow-with-the-Vue-CLI/vue-project/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <header><h1>Tekken 7 Characters</h1></header>
   <section>
+    <header><h1>Tekken 7 Characters</h1></header>
+    <new-friend @add-contact="addContact"></new-friend>
+    <ul>
       <FriendContact
           v-for="friend in friends"
           :key="friend.id"
@@ -11,16 +13,18 @@
           :is-favorite="friend.isFavorite"
           @toggle-favorite="toggleFavoriteStatus"
       />
+    </ul>
 
   </section>
 </template>
 
 <script>
 import FriendContact from "@/components/FriendContact.vue";
+import NewFriend from "@/components/NewFriend.vue";
 
 export default {
   name: "App",
-  components: {FriendContact},
+  components: {NewFriend, FriendContact},
   data() {
     return {
       friends: [
@@ -45,8 +49,17 @@ export default {
     toggleFavoriteStatus(friendId) {
       const friendIdentified = this.friends.find((friend) => friend.id === friendId);
       friendIdentified.isFavorite = !friendIdentified.isFavorite;
+    },
+    addContact(name, email, phone) {
+      const newFriend = {
+        id: new Date().toISOString(),
+        name: name,
+        email: email,
+        phone: phone
+      }
+      this.friends.push(newFriend);
     }
-  }
+  },
 
 };
 </script>
@@ -58,7 +71,7 @@ export default {
 }
 
 html {
-  font-family: "Jost", sans-serif;
+  font-family: 'Jost', sans-serif;
 }
 
 body {
@@ -83,7 +96,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -115,5 +129,21 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+
+#app form div {
+  margin: 1rem 0;
 }
 </style>
