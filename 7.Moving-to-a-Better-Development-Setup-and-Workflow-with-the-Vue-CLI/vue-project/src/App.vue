0ba4a8caecfe8cdc7fe68a1f02b52219cc251a75
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-06-27 18:14:09 +0500

    10. Creating a Basic Vue App

---
+++ 7.Moving-to-a-Better-Development-Setup-and-Workflow-with-the-Vue-CLI/vue-project/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h2>Tekken 7 Characters</h2>
+  <ul>
+    <li></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      friends: [
+        {
+          id: "jin",
+          name: "Jin Kazama",
+          phone: '0321 5214569',
+          email: 'jin.kazama@tekken.com',
+        },
+        {
+          id: "nina",
+          name: "Nina",
+          phone: '+92 345159656',
+          email: 'nina@tekken.com',
+        },
+      ]
+    }
+  },
+
+};
+</script>
+
+
+<style scoped>
+
+</style>
