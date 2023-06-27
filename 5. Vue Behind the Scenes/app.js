380a02f1cda92624f9be3b66795b5bdd9b4b21dd
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-06-27 15:52:19 +0500

    6. Working with Refs

--- 5. Vue Behind the Scenes/app.js
+++ 5. Vue Behind the Scenes/app.js
@@ -1,16 +1,14 @@
 const app = Vue.createApp({
   data() {
     return {
-      currentUserInput: '',
       message: 'Vue is great!',
     };
   },
   methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
-    },
     setText() {
-      this.message = this.currentUserInput;
+      // this.message = this.currentUserInput;
+      // console.dir(this.$refs.userText);
+      this.message = this.$refs.userText.value // it points DOM object of <input type="text" ref="userText">
     },
   },
 });
