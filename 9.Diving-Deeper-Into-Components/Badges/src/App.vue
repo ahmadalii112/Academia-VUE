311e507346427a96bfe45eca0d6d2e0db3c91519
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-12-17 20:41:38 +0500

    3. Global vs Local Components

--- 9.Diving-Deeper-Into-Components/Badges/src/App.vue
+++ 9.Diving-Deeper-Into-Components/Badges/src/App.vue
@@ -11,7 +11,16 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import UserInfo from './components/UserInfo.vue';
+import BadgeList from './components/BadgeList.vue';
+
 export default {
+  components: {
+    TheHeader,
+    UserInfo,
+    BadgeList
+  },
   data() {
     return {
       activeUser: {
