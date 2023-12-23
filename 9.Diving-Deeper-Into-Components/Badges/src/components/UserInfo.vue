db4ea37284a2980f4630fc3f92ddfaee39041547
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-12-23 21:12:14 +0500

    6. Named Slots

--- 9.Diving-Deeper-Into-Components/Badges/src/components/UserInfo.vue
+++ 9.Diving-Deeper-Into-Components/Badges/src/components/UserInfo.vue
@@ -1,24 +1,24 @@
 <template>
   <section>
     <base-card>
-      <header>
+      <template v-slot:header>
         <h3>{{ fullName }}</h3>
         <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-      </header>
-      <p>{{ infoText }}</p>
+      </template>
+      <template v-slot:default>
+        <p>{{ infoText }}</p>
+      </template>
     </base-card>
   </section>
 </template>
 
 <script>
 export default {
-  props: ['fullName', 'infoText', 'role'],
+  props: ["fullName", "infoText", "role"],
 };
 </script>
 
 <style scoped>
-
-
 section header {
   display: flex;
   justify-content: space-between;
