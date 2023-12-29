a1cefbbeb89f993d086d20e67c149b8930bdc79b
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-12-29 23:25:57 +0500

    2. Setup & First Steps

---
+++ 10.Course-Project-The-Learning-Resources-App/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            storedResources: [
+                {id: 'offical-guide', title: 'Official Guide', description: 'The Vue Description' , link: 'https://vue.org' },
+                {id: 'google', title: 'Google', description: 'The Google Description' , link: 'https://www.google.com' },
+            ],
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
